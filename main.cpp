25f45492668eab21b027ad3c8a2567de2a6bc26c
juanlews <juanlews@hotmail.com>
2018-04-09 20:59:08 -0300

    Add funçao printDados

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #include <algorithm>
 #include <iterator>
 #include <sstream>
+#include <time.h>
+#include <locale.h>
+
 
 using namespace std;
 
@@ -25,9 +28,45 @@ struct dado{
     char price[50];
     char property_type[50];
 };
+void printDados(dado *airbnb, int n){
+    for(int i = 0; i < n; i++){
+        printf("\n%i\t\n",
+                airbnb[i].room_id);
+    }
+}
 
+int selecao (dado *v, int n){
+    // menor vai indicar a posiçao que esta o menor elemento, não valor
+    int i, menor, cont=0, j;
+    dado aux;
+    // tam-1. não precisa de ir ate o final do vetor, pois a última posição é o maior
+    for (i = 0; i <n-1; i++){
+        // supondo que o elemento menor esta na primeira posiçao e etc
+        menor=i;
+        // para comparar com a proxima posicao
+        for (j=i+1; j<n; j++){
+             cont++;
+             // recebe em qual posicao o menor esta, não troca
+             if(v[menor].room_id > v[j].room_id){
+                menor = j;
+             }
+             // não compara se é igual, pois  a grande maioria dos dados não é igual.
+             aux = v[i];
+             // passa o elemento menor para primeira posicao
+             v[i] = v[menor];
+             v[menor] = aux;
+        }
+    }
+    printDados(v, n);
+    return cont;
+}
 
-void openFile(int n){
+dado *openFile(int n){
+    //Inicia a contagem do tempo
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    //O código a ter seu tempo de execução medido ficaria neste ponto.
+
     char cabecalho1[50];
     char cabecalho2[50];
     char cabecalho3[50];
@@ -46,12 +85,13 @@ void openFile(int n){
     if(airbnb == NULL){
         cout << "não há memoria suficiente";
     } else {
+        //inicia abertura do arquivo em modo leitura
         FILE *arq = fopen("dados_airbnb.txt", "r");
 
         int i = 0;
 
         for(int a = 0; a <= n; a++){
-
+            //na primeira repetição atribui os valores de cabeçalho
             if(a == 0){
                 fscanf
                     ( arq,
@@ -69,6 +109,7 @@ void openFile(int n){
                         cabecalho10,cabecalho11, cabecalho12
                     );
             }
+            //depois da primeira repetição lê os valores e quarda na Struct
             if(a != 0 ){
                 printf("\n %i %s", a, ")\n " );
                 fscanf
@@ -88,23 +129,31 @@ void openFile(int n){
                 i++;
             }
         }
-
         fclose(arq);
 
     }
 
-
+    Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTempo gasto ao ler o arquivo e atribuir os valores as variáveis: %g ms.", Tempo);
+    getchar();
+    return airbnb;
 }
-int main()
-{int n;
-do{
-    cout<<"Tamanho do Vetor? ";
-    cin>>n;
-}while((n<0) || (n>128001));
+
+int main(){
+
+    setlocale(LC_ALL,"");
+    int n;
+
+    do{
+        cout<<"Tamanho do Vetor? ";
+        cin>>n;
+    } while ((n<0) || (n>128001));
 
     cout<<endl<<endl;
 
-    openFile(n);
+    dado *v = openFile(n);
+    printf("\n%d", selecao(v, n));
 
     return 0;
 }
