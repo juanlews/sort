cf67bfc073d62cc11a51c9d9126ab73c02dd88f1
juanlews <juanlews@hotmail.com>
2018-04-10 01:00:22 -0300

    ordenações prontas

--- main.cpp
+++ main.cpp
@@ -30,42 +30,176 @@ struct dado{
 };
 void printDados(dado *airbnb, int n){
     for(int i = 0; i < n; i++){
-        printf("\n%i\t\n",
-                airbnb[i].room_id);
+        printf("\n%i\t %i\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\n",
+                    airbnb[i].room_id, airbnb[i].host_id, airbnb[i].room_type,
+                    airbnb[i].country, airbnb[i].city, airbnb[i].neighborhood,
+                    airbnb[i].reviews, airbnb[i].overall_satisfaction, airbnb[i].accommodates,
+                    airbnb[i].bedrooms, airbnb[i].price, airbnb[i].property_type);
+
     }
 }
+void bolha(dado *airbnb, int n){
+    /*/Inicia a contagem do tempo
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    //O código a ter seu tempo de execução medido ficaria neste ponto.*/
+    dado temp;
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
 
-int selecao (dado *v, int n){
-    // menor vai indicar a posiçao que esta o menor elemento, não valor
-    int i, menor, cont=0, j;
-    dado aux;
-    // tam-1. não precisa de ir ate o final do vetor, pois a última posição é o maior
-    for (i = 0; i <n-1; i++){
-        // supondo que o elemento menor esta na primeira posiçao e etc
-        menor=i;
-        // para comparar com a proxima posicao
-        for (j=i+1; j<n; j++){
-             cont++;
-             // recebe em qual posicao o menor esta, não troca
-             if(v[menor].room_id > v[j].room_id){
-                menor = j;
-             }
-             // não compara se é igual, pois  a grande maioria dos dados não é igual.
-             aux = v[i];
-             // passa o elemento menor para primeira posicao
-             v[i] = v[menor];
-             v[menor] = aux;
+            if (airbnb[j].room_id > airbnb[j+1].room_id){
+                temp = airbnb[j];
+                airbnb[j] = airbnb[j+1];
+                airbnb[j+1] = temp;
+            }
         }
     }
-    printDados(v, n);
-    return cont;
+    /*Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTempo gasto ao ordenar por BubbleSort: %g ms.", Tempo);
+    getchar();*/
+}
+void selecao(dado *airbnb, int n){//SELECAO
+    /*/Inicia a contagem do tempo
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    //O código a ter seu tempo de execução medido ficaria neste ponto.*/
+    int i, j, menor;
+    dado temp;
+    for (i = 0; i < n-1; i++){
+        menor = i;
+        for (j = i + 1; j < n; j++){
+            if (airbnb[j].room_id < airbnb[menor].room_id){
+                menor = j;
+            }
+        }
+        temp = airbnb[menor];
+        airbnb[menor] = airbnb[i];
+        airbnb[i] = temp;
+    }
+    /*Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTempo gasto ao ordenar por SelectionSort: %g ms.", Tempo);
+    getchar();*/
+}
+void merges(dado *airbnb, int l, int m, int r){
+
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    /* create temp arrays */
+    dado L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = airbnb[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = airbnb[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (L[i].room_id <= R[j].room_id)
+        {
+            airbnb[k] = L[i];
+            i++;
+        }
+        else
+        {
+            airbnb[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        airbnb[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        airbnb[k] = R[j];
+        j++;
+        k++;
+    }
+
 }
 
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(dado *airbnb, int l, int r, int toTime = 0){
+
+    if (l < r){
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+
+        // Sort first and second halves
+        mergeSort(airbnb, l, m , 1);
+        mergeSort(airbnb, m+1, r, 1);
+
+        merges(airbnb, l, m, r);
+    }
+}
+
+void quick_sort(dado *airbnb, int left, int right) {
+    /*/Inicia a contagem do tempo
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    //O código a ter seu tempo de execução medido ficaria neste ponto.*/
+    int i, j;
+    dado x, y;
+
+    i = left;
+    j = right;
+    x = airbnb[(left + right) / 2];
+
+    while(i <= j) {
+        while(airbnb[i].room_id < x.room_id && i < right) {
+            i++;
+        }
+        while(airbnb[j].room_id > x.room_id && j > left) {
+            j--;
+        }
+        if(i <= j) {
+            y = airbnb[i];
+            airbnb[i] = airbnb[j];
+            airbnb[j] = y;
+            i++;
+            j--;
+        }
+    }
+
+    if(j > left) {
+        quick_sort(airbnb, left, j);
+    }
+    if(i < right) {
+        quick_sort(airbnb, i, right);
+    }
+
+    /*Ticks[1] = clock();
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\nTempo gasto ao ordenar com QuickSort: %g ms.", Tempo);
+    getchar();*/
+}
 dado *openFile(int n){
-    //Inicia a contagem do tempo
+
+    /*/Inicia a contagem do tempo
     clock_t Ticks[2];
     Ticks[0] = clock();
     //O código a ter seu tempo de execução medido ficaria neste ponto.
+    */
 
     char cabecalho1[50];
     char cabecalho2[50];
@@ -133,10 +267,10 @@ dado *openFile(int n){
 
     }
 
-    Ticks[1] = clock();
+    /*Ticks[1] = clock();
     double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
     printf("\nTempo gasto ao ler o arquivo e atribuir os valores as variáveis: %g ms.", Tempo);
-    getchar();
+    getchar();*/
     return airbnb;
 }
 
@@ -153,8 +287,8 @@ int main(){
     cout<<endl<<endl;
 
     dado *v = openFile(n);
-    printf("\n%d", selecao(v, n));
-
+    bolha(v, n);
+    printDados(v, n);
     return 0;
 }
 
